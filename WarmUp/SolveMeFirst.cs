d5738194ddab58104cc927b451b0f8d6c0aa603c
siddharthadas1991 <siddharthadas1991@gmail.com>
2021-06-21 17:43:58 +0530

    21/06/2021

--- WarmUp/SolveMeFirst.cs
+++ WarmUp/SolveMeFirst.cs
@@ -1,15 +1,4 @@
-//Question : - Complete the function solveMeFirst to compute the sum of two integers.
- //Function Description
-//Complete the solveMeFirst function in the editor below.
-//solveMeFirst has the following parameters:
-
-//int a: the first value
-//int b: the second value
-//Returns
-//- int: the sum of  and 
-
-//Sample Input a = 2 b = 3       Sample Output :5 
-
+ 
 using System;
 using System.Collections.Generic;
 using System.IO;
